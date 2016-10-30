69de9d210d60ec5a3558d800984c61974df6531d
somenugget <some.nugget@gmail.com>
2016-10-30 15:46:13 +0200

    Fix events routes

--- lits/config/routes.rb
+++ lits/config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :events, only: [:index, :show] do
-    get 'date/:date', to: 'events#date', as: 'events_date'
-  end
+  resources :events, only: [:index, :show]
+  
+  get 'events/date/:date', to: 'events#date', as: 'date'
 
   root 'events#index'
 end
