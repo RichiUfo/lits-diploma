51cb58384022879e5c1a06a4e5c2eade3d8d2bc8
somenugget <some.nugget@gmail.com>
2016-11-04 00:08:54 +0200

    selecting needed receiver by source type name

---
+++ lits/lib/components/events_receiver/receiver_factory.rb
@@ -0,0 +1,25 @@
+module Components
+  module EventsReceiver
+    class ReceiverFactory
+      def self.source_receiver source_type_name
+        if SourceType::KEYS[source_type_name.downcase.to_sym].blank?
+          raise ArgumentError, "Source type with id #{source_id} doesnt exist" 
+        end
+
+        receiver_class_name = self.name.sub('ReceiverFactory', source_type_name.capitalize.to_s + 'Receiver')
+        receiver_class_name.constantize.instance
+      end
+
+      def self.load_events_of_source_type source_type_name
+        receiver = source_receiver source_type_name
+        sources  = Source.where(source_type_id: SourceType::KEYS[source_type_name.downcase.to_sym]).all
+
+        sources.each do |source|
+          receiver.save_source_events source
+        end
+
+        receiver.report
+      end 
+    end
+  end
+end
