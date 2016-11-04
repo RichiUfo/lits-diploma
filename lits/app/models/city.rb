3a0c799d628e83383ec8907deeb51086b4fe2bfc
somenugget <some.nugget@gmail.com>
2016-11-04 20:00:10 +0200

    Simplify city search by source external id

--- lits/app/models/city.rb
+++ lits/app/models/city.rb
@@ -2,7 +2,6 @@ class City < ApplicationRecord
   has_many :events
 
   def self.find_by_vk_id vk_id
-    city_source_type = CitySourceType.find_by(source_type: SourceType::KEYS[:vk], ext_id: vk_id)
-    city_source_type.nil? ? nil : city_source_type.city  
+    CitySourceType.find_by(source_type: SourceType::KEYS[:vk], ext_id: vk_id).try(:city)  
   end
 end
