75d12f4862a8d292a2517211e207d592c035fce6
yuri-val <yuri.valigursky@gmail.com>
2016-11-04 17:32:06 +0200

    dou..

--- lits/lib/components/events_receiver/dou_receiver.rb
+++ lits/lib/components/events_receiver/dou_receiver.rb
@@ -5,8 +5,10 @@ module Components
     class DouReceiver < BaseReceiver
       SOURCE_EVENTS_URL = URI('https://dou.ua/calendar/feed/%D0%B2%D1%81%D0%B5%20%D1%82%D0%B5%D0%BC%D1%8B%2F%D0%9E%D0%B4%D0%B5%D1%81%D1%81%D0%B0')
 
+      attr_reader
+
       def self.run
-        @dou_reader ||= self.new
+        @dou_reader ||= new
         @dou_reader.process_data
       end
 
@@ -15,73 +17,69 @@ module Components
         fetch_data_from_xml
       end
 
-      def data
-        @data
-      end
-
-
       def process_data
-        raw_events = @data["rss"]["channel"]["item"]
+        raw_events = @data['rss']['channel']['item']
         raw_events.each do |e|
           raw_event = {}
-          raw_event["title"] = e["title"]
-          #raw_event["city"],
-          raw_event["address"] = get_event_date_time_adress e["description"].lines.first
+          raw_event['title'] = e['title']
+          # raw_event["city"],
+          raw_event['address'] = get_event_date_time_adress e['description'].lines.first
 
-          p raw_event["address"]
-          #p e["description"].lines.first
+          p raw_event['address']
+          # p e["description"].lines.first
         end
       end
 
-
-      def prepare_event raw_event
+      def prepare_event(raw_event)
         {
           city_id:      city_id,
-          organizer_id: "",
-          category_id:  "",
-          source_id:    "",
-          format_id:    "",
-          date:         "",
-          price:        "",
-          coordinates:  "",
-          ext_id:       "",
-          name:         raw_event["title"],
-          picture:      "",
-          description:  "",
-          address:      "",
-          reg_ref:      ""
+          organizer_id: '',
+          category_id:  '',
+          source_id:    '',
+          format_id:    '',
+          date:         '',
+          price:        '',
+          coordinates:  '',
+          ext_id:       '',
+          name:         raw_event['title'],
+          picture:      '',
+          description:  '',
+          address:      '',
+          reg_ref:      ''
         }
       end
 
-      def save_event prepared_event
+      def save_event(prepared_event)
         Event.Create prepared_event
       end
 
-
       protected
+
       def fetch_data_from_xml
-        @data = Hash.from_xml(Net::HTTP.get SOURCE_EVENTS_URL)
+        # @data = Hash.from_xml(Net::HTTP.get(SOURCE_EVENTS_URL))
+        @data = Hash.from_xml(Rails.root.join('lib', 'components', 'events_receiver', 'debug_xml.xml'), &:read)
       end
 
-      def city_id adress
+      def city_id(city_strind)
         city_name = city_strind.split(',')[0]
-        city = City.find_by_name ext_city_id
+        city = City.find_by(name: city_name)
         city.present? ? city.id : nil
       end
 
-      def get_event_date_time_adress html_string
+      def get_event_date_time_adress(html_string)
         @first_string = Nokogiri::HTML(html_string)
         @first_string.children.children.children.children.children[8]
-        #adr_parts = full_adress.split("")
-        @first_string.nil? ? "" : prepare_date_time_adress(@first_string.content)
+        # adr_parts = full_adress.split("")
+        @first_string.nil? ? '' : prepare_date_time_adress(@first_string.content)
       end
 
-      def prepare_date_time_adress string
-        string.gsub!("Время:", "Время:\n").gsub!("Час:", "Час:\n")
-        string.gsub!("Место:", "Место:\n").gsub!("Місце:", "Місце:\n")
-        return string
+      def prepare_date_time_adress(string)
+        words = ['Время:', 'Час:', 'Место:', 'Місце:']
+        words.each do |word|
+          string.gsub!(word, "#{word}\n") unless string.index(word).nil?
+        end
+        string
       end
-
     end
   end
 end
