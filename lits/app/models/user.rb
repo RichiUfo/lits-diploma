7b3d5ab1d4d2a16fa1b3015515962011ac270528
somenugget <some.nugget@gmail.com>
2016-11-13 15:39:11 +0200

    Show user panel in navbar for authenticated user

--- lits/app/models/user.rb
+++ lits/app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-
-  devise :trackable, :omniauthable, :omniauth_providers => [:facebook, :vkontakte]
+  devise :trackable, :omniauthable, omniauth_providers: [:facebook, :vkontakte]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -9,4 +8,14 @@ class User < ApplicationRecord
     end
   end
 
+  def avatar
+    if provider == 'vkontakte'
+      client = VkontakteApi::Client.new
+      client.users.get(user_id: uid, fields: 'photo_50').first[:photo_50]
+    elsif provider == 'facebook'
+      "http://graph.facebook.com/#{uid}/picture?type=square"
+    else
+      ''
+    end
+  end
 end
--- lits/config/routes.rb
+++ lits/config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks'}
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
+
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   resources :events, only: [:index, :show] do
     get 'date/:date', to: 'events#date', on: :collection
   end
+
+  get 'feed', to: 'feed#index', as: :feed
+  get 'feed/edit', to: 'feed#edit', as: :feed_edit
 
   root 'events#index'
 end
