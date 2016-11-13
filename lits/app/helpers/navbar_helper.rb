8f3771a52f9d10eec2fc6df7e385147710003e6b
somenugget <some.nugget@gmail.com>
2016-11-13 15:33:10 +0200

    Fix rubocop warnings

--- lits/app/helpers/navbar_helper.rb
+++ lits/app/helpers/navbar_helper.rb
@@ -1,13 +1,14 @@
 module NavbarHelper
-  def navbar header, links, &block
-    content_tag(:nav, class: 'navbar navbar-inverse navbar-fixed-top') do 
-      header = navbar_header(header) 
-      
-      collapse = if block_given?
-        navbar_collapse(links){ yield }
-      else
-        navbar_collapse(links)
-      end
+  def navbar(header, links)
+    content_tag(:nav, class: 'navbar navbar-inverse navbar-fixed-top') do
+      header = navbar_header(header)
+
+      collapse =
+        if block_given?
+          navbar_collapse(links) { yield }
+        else
+          navbar_collapse(links)
+        end
 
       container(header + collapse)
     end
@@ -15,38 +16,40 @@ module NavbarHelper
 
   private
 
-  def navbar_header header
+  def navbar_header(header)
     content_tag :div, navbar_brand(header) + navbar_toggle, class: 'navbar-header'
   end
 
-  def navbar_brand header
-    link_to header, root_path, {class: 'navbar-brand'}
+  def navbar_brand(header)
+    link_to header, root_path, class: 'navbar-brand'
   end
-  
+
   def navbar_toggle
-    button_tag({class: 'navbar-toggle', 
-                'aria-expanded' => false,
-                'aria-controls' => 'navbar',
-                data: {toggle: 'collapse', target: '#navbar-main' }}) do
-        content_tag(:span, '', class: 'icon-bar') +
-        content_tag(:span, '', class: 'icon-bar') +
-        content_tag(:span, '', class: 'icon-bar') 
+    button_tag(class: 'navbar-toggle',
+               'aria-expanded' => false,
+               'aria-controls' => 'navbar',
+               data: { toggle: 'collapse', target: '#navbar-main' }) do
+      concat content_tag(:span, '', class: 'icon-bar')
+      concat content_tag(:span, '', class: 'icon-bar')
+      concat content_tag(:span, '', class: 'icon-bar')
     end
   end
 
-  def navbar_collapse links, &block
+  def navbar_collapse(links, &block)
     content_tag :div, class: 'navbar-collapse collapse', id: 'navbar-main' do
-      lis = links.keys.map do |key| 
-        content_tag :li, link_to(key, links[key]), class: (current_page?(links[key])) ? 'active' : ''
-      end
-
-      content = content_tag(:ul, lis.join.html_safe, {class: 'nav navbar-nav'});
+      content = content_tag(:ul, navbar_links(links), class: 'nav navbar-nav')
 
       if block_given?
-        content = content + content_tag(:ul, capture(&block), {class: 'nav navbar-nav navbar-right'})      
+        content += content_tag(:ul, capture(&block), class: 'nav navbar-nav navbar-right')
       end
 
       content
     end
-  end  
+  end
+
+  def navbar_links(links)
+    links.keys.map do |key|
+      content_tag :li, link_to(key, links[key]), class: current_page?(links[key]) ? 'active' : ''
+    end
+  end
 end
