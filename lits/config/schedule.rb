bc85fd71dd3a193059547a8f16c5a7b273a2cfa2
somenugget <some.nugget@gmail.com>
2016-11-24 10:59:13 +0200

    Fix schedule

--- lits/config/schedule.rb
+++ lits/config/schedule.rb
@@ -1,4 +1,4 @@
-every '* * * * *' do
+every 1.day, at: '4:00 am' do
   rake 'receive_events:run', output: { standard: '~/.log/receive_events.log',
                                        error: '~/.log/receive_events_errors.log' }
 end
