e243400a8caaff0d8879d970922909747980ab49
yuri-val <yuri.valigursky@gmail.com>
2016-11-26 23:52:40 +0200

    ugly tag cloud, events by tag

--- lits/app/models/tag.rb
+++ lits/app/models/tag.rb
@@ -3,7 +3,12 @@ class Tag < ApplicationRecord
   has_many :event_tags
   has_many :events, through: :event_tags
   friendly_id :name, use: :slugged
+
   def normalize_friendly_id(text)
     text.to_slug.transliterate(:russian).normalize.to_s
+  end
+
+  def self.number
+    self.select("name, slug, count(event_tags.tag_id) as count").joins(:event_tags).group("name, slug, event_tags.tag_id")
   end
 end
