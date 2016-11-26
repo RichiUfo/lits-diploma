23b0061237168cf505fc1ff07c463b602e93e46b
somenugget <some.nugget@gmail.com>
2016-11-26 12:12:37 +0200

    Changes in models

--- lits/app/models/source_type.rb
+++ lits/app/models/source_type.rb
@@ -1,13 +1,4 @@
 class SourceType < ApplicationRecord
-  KEYS = {
-    vk: 1,
-    fb: 2,
-    dou: 3
-  }
-
-  NAMES = {
-    1 => :vk,
-    2 => :fb,
-    3 => :dou,
-  }
+  KEYS = { vk: 1, fb: 2, dou: 3 }.freeze
+  NAMES = { 1 => :vk, 2 => :fb, 3 => :dou }.freeze
 end
