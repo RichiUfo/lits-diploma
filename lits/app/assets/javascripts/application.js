b658420d2bbdb1c5c642515d241fc7911781e315
somenugget <some.nugget@gmail.com>
2016-12-03 16:36:33 +0200

    Added on top button. Refatored application.js

--- lits/app/assets/javascripts/application.js
+++ lits/app/assets/javascripts/application.js
@@ -7,36 +7,66 @@
 //= require sidebar
 
 $(document).ready(function() {
-  var $sideBar = $('.ui.sidebar').sidebar({
-    transition: 'overlay', 
-  }),
-  $masonryContainer = $('#masonry-container'),
-  $sidebarToggle = $('.sidebar-toggle');
-
-  executeAfterLoading($masonryContainer.find('img'), function(){
-    $masonryContainer.masonry({
-      itemSelector: '.box',
-      isAnimated: !Modernizr.csstransitions,
-      isFitWidth: true
-    });
-  });
-
-  $sidebarToggle.on('click', function(e) {
-    e.preventDefault();
-    $sideBar.sidebar('toggle');
-  });
+  register.onTopButton();
+  register.sideBarToggle();
+  register.masonry();
 });
 
+var $sideBar = $('.ui.sidebar').sidebar({ transition: 'overlay' }),
+    $masonryContainer = $('#masonry-container'),
+    $sidebarToggle = $('.sidebar-toggle'),
+    $onTopButton = $('#on-top-button');
 
-function executeAfterLoading($jQobject, callback) {
-  var total = $jQobject.length,
-    loaded = 0;
+var register = {
+  masonry: function () {
+    helper.executeAfterLoading($masonryContainer.find('img'), function(){
+      $masonryContainer.masonry({
+        itemSelector: '.box',
+        isAnimated: !Modernizr.csstransitions,
+        isFitWidth: true
+      });
+    });
+  },
 
-  $jQobject.on('load', function() {
-    loaded++;
+  onTopButton: function () {
+    var button_visible = 300, 
+      delay = 1000;
 
-    if (total == loaded) {
-      callback();
-    }
-  });
-}
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > button_visible) {
+        $onTopButton.fadeIn();
+      } else {
+        $onTopButton.fadeOut();
+      }
+    });
+
+    $onTopButton.click(function () {
+      $('body, html').animate({
+        scrollTop: 0
+      }, delay);
+    });
+  },
+  
+  sideBarToggle: function () {
+    $sidebarToggle.on('click', function(e) {
+      e.preventDefault();
+      $sideBar.sidebar('toggle');
+    });
+  }
+};
+
+var helper = {
+  executeAfterLoading: function ($jQobject, callback) {
+    var total = $jQobject.length,
+      loaded = 0;
+
+    $jQobject.on('load', function() {
+      loaded++;
+
+      if (total == loaded) {
+        callback();
+      }
+    });
+  }
+};
+
