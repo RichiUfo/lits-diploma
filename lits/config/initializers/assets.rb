27316ab4913a3aab9bc9c10177c07537d61fd17c
somenugget <some.nugget@gmail.com>
2016-12-03 15:08:03 +0200

    Fix precompiling for admin

--- lits/config/initializers/assets.rb
+++ lits/config/initializers/assets.rb
@@ -1,5 +1,4 @@
 # Be sure to restart your server when you modify this file.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.paths << File.join(Rails.root, 'vendor/assets/flat-ui/css')
-Rails.application.config.assets.paths << File.join(Rails.root, 'vendor/assets/flat-ui/js')
+Rails.application.config.assets.precompile += %w(admin/admin.css admin/admin.js)
