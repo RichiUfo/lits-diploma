1555acb0b1a7c960d13ae6201b78c7ed836671ba
somenugget <some.nugget@gmail.com>
2016-12-08 15:18:28 +0200

    Fix spelling error

--- lits/lib/components/meta.rb
+++ lits/lib/components/meta.rb
@@ -2,7 +2,7 @@ module Components
   module Meta
     APP_NAME = Rails.application.config.app_name
     GLUE = ' | '.freeze
-    APP_DESCRIPTION = "Сайт #{APP_NAME} поможет вам насти место, " \
-                      'где вы с интересом и пользой можете провести время в Одессе' 
+    APP_DESCRIPTION = "Сайт #{APP_NAME} поможет вам найти место, " \
+                      'где вы с интересом и пользой можете провести время в Одессе'.freeze
   end
 end
